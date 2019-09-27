86610a06ab64623a5c002af662b675cfeb0c5278
Craig Fox <cwfox@arkbluecross.com>
2019-09-27 13:25:42 -0500

    setting up repo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GamesLegacy msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { XlsCsvParser } from 'vue-xls-csv-parser';
+import GamesLegacy from './components/GamesLegacy.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    XlsCsvParser,
+    GamesLegacy
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
