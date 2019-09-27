5883b80c77cbd200e6a811f2ad556f38c8005cc5
Craig Fox <cwfox@arkbluecross.com>
2019-09-27 15:52:46 -0500

    troubleshooting papa parse issues

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="app">
     <h1>Games Lists</h1>
+    <GameTable tableName="Games Legacy"></GameTable>
   </div>
 </template>
 
 <script>
-import Papa from 'papaparse'
+import Papa from 'papaparse';
+import GameTable from './components/GameTable.vue';
 
 export default {
   name: 'app',
   components: {
+    GameTable
+  },
+  data() {
+    return {
+      objGamesLegacy: {'dataobj': 'data'}
+    }
+  },
+  mounted() {
+    this.objGamesLegacy = {
+      'mount': 'mounted'
+    };
+
+    Papa.parse('./assets/games-played-legacy.csv', {
+      complete: function(results) {
+        window.console.log(results);
+
+        this.objGamesLegacy = {
+          'parse': 'parsed'
+        };
+      }
+    });
   }
 }
 </script>
---
+++ src/components/GameTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="game-table">
+    <h2>{{tableName}}</h2>
+    <table>
+      <thead>
+        <tr>
+          <!-- <th v-for="value in testing">
+            {{ value }}
+          </th> -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr></tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GamesLegacy',
+  props: {
+    tableName: String,
+    tableCsv: Object
+  }
+}
+</script>
+
+<style scoped>
+</style>
