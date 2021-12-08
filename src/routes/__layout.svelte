07e3915d7070a7acc53ac551d6e5fe268b08f4fb
Craig Fox <cwfox@arkbluecross.com>
2021-12-08 08:26:25 -0600

    added google fonts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,13 @@
 </script>
 
 <svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;500;700&display=swap"
+		rel="stylesheet"
+	/>
+
 	<style lang="scss">
 		/* ====---------------====
 			Support
