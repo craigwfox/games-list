6b2116e973f14c29d21546cb5d1ad19ca4961f4b
Craig Fox <cwfox@arkbluecross.com>
2021-12-08 07:47:46 -0600

    added sass from games-list v1

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,11 @@ const config = {
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},preprocess: preprocess({
+		scss: {
+			prependData: `@import './src/scss/styles.scss';`
+		}
+ })
 };
 
 export default config;
