868f170f848b47e83b6d8e8d6335350d62366835
Craig Fox <cwfox@arkbluecross.com>
2021-12-08 16:27:10 -0600

    added filtering html

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,30 @@
   <title>Home - Games list</title>
 </svelte:head>
 
-<section>
-  <h1>This is a page of pagey stuff</h1>
+<section aria-labelledby="recent-games">
+  <h1 id="recent-games">Recent games</h1>
+
+  <div class="game-filters">
+    <div class="form">
+      <label for="sortYear">Sort by year</label>
+      <select name="sortYear" id="sortYear">
+        <option value="all" selected>All</option>
+        <option value="">2021</option>
+      </select>
+    </div>
+    <div class="form">
+      <label for="sortConsole">Sort by console</label>
+      <select name="sortConsole" id="sortConsole">
+        <option value="all" selected>All</option>
+        <option value="">SNES</option>
+      </select>
+    </div>
+    <div class="form">
+      <label for="sortRating">Sort by rating</label>
+      <select name="sortRating" id="sortRating">
+        <option value="all" selected>All</option>
+        <option value="">bad</option>
+      </select>
+    </div>
+  </div>
 </section>
