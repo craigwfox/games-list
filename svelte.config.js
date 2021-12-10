b54317d8452142aa86bce7402ee199920d06e3ed
Craig Fox <cwfox@arkbluecross.com>
2021-12-10 14:40:02 -0600

    moving games around and working on markdown preprocessing

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,24 @@
 import svelte from 'rollup-plugin-svelte';
 import adapter from '@sveltejs/adapter-netlify';
 import sveltePreprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
-	preprocess: sveltePreprocess()
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte'
+  },
+  preprocess: sveltePreprocess(),
+  plugins: [
+    svelte({
+      // tell svelte to handle mdsvex files
+      extensions: ['.svelte', '.svx', '.md'],
+      preprocess: mdsvex({ extensions: ['.svx', '.md'] })
+    })
+  ]
 };
 
 export default config;
