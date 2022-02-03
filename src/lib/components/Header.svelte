20315836f2e897bc3762b4b640d7cf21099c6844
Craig Fox <craig@craigwfox.com>
2022-02-03 11:35:31 -0600

    reworked header to have a logo mark and scaling type

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,9 +3,10 @@
 </script>
 
 <header class="site-header">
-  <h1>🎮 Games list</h1>
-  <nav class="site-nav">
-    <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/">Home</a>
+  <img class="sh__mark" src="./images/snes-controller-vert.svg" alt="">
+  <h1 class="sh__title">Games list</h1>
+  <nav class="sh__nav">
+    <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/">Stats</a>
     <a class:active={$page.url.pathname === '/games'} sveltekit:prefetch href="/games">Games</a>
     <a class:active={$page.url.pathname === '/about'} sveltekit:prefetch href="/about">About</a>
   </nav>
