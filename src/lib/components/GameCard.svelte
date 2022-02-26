3c17e4d18422e5312896a4c85969525af3999bc0
Craig Fox <craig@craigwfox.com>
2022-02-26 15:21:42 -0600

    added currently playing to gameCard

--- src/lib/components/GameCard.svelte
+++ src/lib/components/GameCard.svelte
@@ -1,28 +1,40 @@
 <script>
   // Imports
-  import {format} from '$lib/formating';
+  import { format } from '$lib/formating';
 
   // Component props
-  export let game
-  export let index
+  export let game;
+  export let data = game.meta;
+  export let index;
 </script>
 
 <article class="game-card">
   <a href={game.path}>
     <h2 id={`title-${index}`} class="gc__title">
-      {game.meta.title}
+      {data.title}
     </h2>
-    <img class="gc__img" src={game.meta.game_info.poster} alt={`Game cover for ${game.meta.title}`} />
+    <img
+      class="gc__img"
+      src={data.game_info.poster}
+      alt={`Game cover for ${data.title}`}
+    />
     <ul class="gc__details">
       <li>
         <strong>Played on</strong>
-        {format.prettyLabel(game.meta.console_settings.console[0], format.consoleArry)}
+        {format.prettyLabel(
+          data.console_settings.console[0],
+          format.consoleArry
+        )}
       </li>
       <li>
-        <strong>Played in</strong>
-        {#each game.meta.game_info.times_played as play}
-        <span>{play.date_year}</span>
-        {/each}
+        {#if data.play_status === 'false'}
+          <span class="gc__details_playing">Currently playing</span>
+        {:else}
+          <strong>Played in</strong>
+          {#each data.game_info.times_played as play}
+            <span>{play.date_year}</span>
+          {/each}
+        {/if}
       </li>
     </ul>
   </a>
