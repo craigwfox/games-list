8006dfd7cb9bb2e5616b1cdaa24c047ac51967a4
Craig Fox <craig@craigwfox.com>
2022-02-26 22:34:29 -0600

    removed test calls

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,5 @@
 <script>
   import { seo } from '$lib/store.js';
-
-  console.log('meow');
-  async function getDetails() {
-    console.log('running');
-    try {
-      const response = await fetch(
-        `/netlify/functions/gameDetails?game=astroneer`
-      );
-      // const res = await response.json();
-      // const data = await res;
-      // details = data;
-      console.log(response);
-      return response;
-    } catch (err) {
-      console.log(`Game.svelte: ${err}`);
-    }
-  }
-  getDetails();
 </script>
 
 <svelte:head>
@@ -25,30 +7,3 @@
 </svelte:head>
 
 <h1 id="recent-games">Recent games</h1>
-
-<section aria-label="Games feed">
-  <div class="game-filters">
-    <div class="form">
-      <label for="sortYear">Sort by year</label>
-      <select name="sortYear" id="sortYear">
-        <option value="all" selected>All</option>
-        <option value="">2021</option>
-      </select>
-    </div>
-    <div class="form">
-      <label for="sortConsole">Sort by console</label>
-      <select name="sortConsole" id="sortConsole">
-        <option value="all" selected>All</option>
-        <option value="">SNES</option>
-      </select>
-    </div>
-    <div class="form">
-      <label for="sortRating">Sort by rating</label>
-      <select name="sortRating" id="sortRating">
-        <option value="all" selected>All</option>
-        <option value="">bad</option>
-      </select>
-    </div>
-  </div>
-  <div class="game-wrapper">...</div>
-</section>
