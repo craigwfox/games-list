5166c8aed18e021fb20df6b7a324dd133d6b9b0e
Craig Fox <craig@craigwfox.com>
2022-02-28 07:51:00 -0600

    swapped to an array

--- src/routes/api/stats.json.js
+++ src/routes/api/stats.json.js
@@ -3,19 +3,42 @@ export const get = async () => {
   const allGameFiles = import.meta.glob('../games/*.md');
   const iterableGameFiles = Object.entries(allGameFiles);
 
-  const allGames = await Promise.all(
+  let stats = [];
+
+  // ===------ takes an array and checks it's children for a match ------===
+  const includesChild = (arry, val) => {
+    let match = false;
+    arry.forEach((child) => {
+      if (child.hasOwnProperty(val)) {
+        console.log(child);
+        match = true;
+        return true;
+      }
+    });
+    return match;
+  };
+
+  const allStats = await Promise.all(
     iterableGameFiles.map(async ([path, resolver]) => {
       const { metadata } = await resolver();
-      const GamePath = path.slice(2, -3);
+      const date = metadata.game_info.times_played[0].date_year;
 
-      return {
-        meta: metadata,
-        path: GamePath
-      };
+      if (includesChild(stats, date)) {
+        console.log('has match');
+        stats[date].count++;
+      } else {
+        stats.push({
+          label: [date][0],
+          count: 1
+        });
+      }
     })
   );
 
   return {
-    body: allGames
+    body: {
+      years: stats,
+      consoles: []
+    }
   };
 };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,11 @@
 <script context="module">
   export const load = async ({ fetch }) => {
-    const games = await fetch('/api/games.json');
-    const allGames = await games.json();
+    const stats = await fetch('/api/stats.json');
+    const allStats = await stats.json();
 
     return {
       props: {
-        games: allGames
+        stats: allStats
       }
     };
   };
@@ -16,51 +16,12 @@
   // Imports
   // ====---------------====
   import { seo } from '$lib/store.js';
-  import { format } from '$lib/formating';
 
   // ====---------------====
   // Variables
   // ====---------------====
-  export let games;
-
-  // ====---------------====
-  // Helpers
-  // ====---------------====
-
-  // ===------ takes an array and checks it's children for a match ------===
-  const includesChild = (arry, val) => {
-    let match = false;
-    arry.some((child) => {
-      if (child.includes(val)) {
-        match = true;
-        return true;
-      }
-    });
-    return match;
-  };
-
-  // ====---------------====
-  // Gamecount by years
-  // ====---------------====
-  const gcYears = [];
-  const gameCountYear = () => {
-    games.forEach((game) => {
-      const firstYear =
-        game.meta.game_info.times_played[0].date_year.toLowerCase();
-
-      if (includesChild(gcYears, firstYear)) {
-        let pos;
-        gcYears.some((child, index) => {
-          pos = index;
-        });
-        gcYears[pos][1] = gcYears[pos][1] + 1;
-      } else {
-        gcYears.push([firstYear, 1]);
-      }
-    });
-  };
-  gameCountYear();
-  $: gcYearsAsc = gcYears.sort(format.sortYears);
+  export let stats;
+  const years = [];
 </script>
 
 <svelte:head>
@@ -72,10 +33,10 @@
 <h2>Play totals by year</h2>
 <table>
   <tbody>
-    {#each gcYears as year}
+    {#each years as year}
       <tr>
-        <th scope="row">{year[0]}</th>
-        <td>{year[1]}</td>
+        <th scope="row">{year.label}</th>
+        <td>{year.count}</td>
       </tr>
     {/each}
   </tbody>
